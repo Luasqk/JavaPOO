460c2db09645b51be650b5392d0256741e3b3d50
Luasqk <palarobrandaolucas@edu.unifil.br>
2026-03-31 13:40:17 -0300

    atividade de modificadores de acesso

---
+++ src/EX05/TesteFuncionario.java
@@ -0,0 +1,16 @@
+package EX05;
+
+public class TesteFuncionario {
+    public static void main(String[] args) {
+        Funcionario f = new Funcionario();
+
+        f.setSetor("Financeiro");
+        f.setCargo("Analista");
+        f.setEmpresa("AlphaTech");
+        f.setSalario(3500.0);
+
+        System.out.println("Setor: " + f.getSetor());
+        System.out.println("Cargo: " + f.getCargo());
+        System.out.println("Empresa: " + f.getEmpresa());
+    }
+}
